258f5d973396b7851ff23194f34f1dd6befa80c6
InsaneArtificer <JMStrahm97@gmail.com>
2025-03-02 21:34:10 -0600

    leafoen, jolteon terrains try 2

--- src/js/shared_controls.js
+++ src/js/shared_controls.js
@@ -631,6 +631,18 @@ function autosetTerrain(ability, i) {
 		$("#psychic").prop("checked", true);
 		break;
 	default:
+		// Rainbow - leafeon sets grassy terrain
+		if (chromaticField === 'Rainbow' && pokemonName == 'Leafon') {
+			lastAutoTerrain[i] = "Grassy";
+			$("#grassy").prop("checked", true);
+			break;
+		}
+		// Rainbow - Jolteon sets electric terrain
+		if (chromaticField === 'Rainbow' && pokemonName == 'Jolteon') {
+			lastAutoTerrain[i] = "Electric";
+			$("#electric").prop("checked", true);
+			break;
+		}
 		// If no terrain setting it sets last
 		lastAutoTerrain[i] = "";
 		var newTerrain = lastAutoTerrain[1 - i] !== "" ? lastAutoTerrain[1 - i] : lastManualTerrain;
@@ -639,18 +651,7 @@ function autosetTerrain(ability, i) {
 		}
 		break;
 	}
-	// Rainbow - leafeon sets grassy terrain
-	//if (chromaticField === 'Rainbow' && species == 'Leafon') {
-	//	lastAutoTerrain[i] = "Grassy";
-	//	$("#grassy").prop("checked", true);
-	//	break;
-	//}
-	// Rainbow - Jolteon sets electric terrain
-	//if (chromaticField === 'Rainbow' && species == 'Jolteon') {
-	//	lastAutoTerrain[i] = "Electric";
-	//	$("#electric").prop("checked", true);
-	//	break;
-	//}
+
 }
 
 $("#p1 .item").bind("keyup change", function () {
