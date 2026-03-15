eb32663f4b001eceabae10bcb0fdc57cd6272b18
InsaneArtificer <JMStrahm97@gmail.com>
2026-03-15 11:55:11 -0500

    making fields current and failing to get the bug protosynthesis working

--- calc/src/mechanics/util.ts
+++ calc/src/mechanics/util.ts
@@ -818,10 +818,11 @@ export function isQPActive(
 
   const weather = field.weather || '';
   const terrain = field.terrain;
-
+  
   return (
-    ((pokemon.hasAbility('Protosynthesis') || pokemon.named('Druddigon-Crest')) && // Druddigon Crest - Grants Protosynthesis
-      (weather.includes('Sun') || pokemon.hasItem('Booster Energy'))) ||
+    ((pokemon.hasAbility('Protosynthesis') || pokemon.named('Druddigon-Crest')) || // Druddigon Crest - Grants Protosynthesis
+    (field.chromaticField === 'Jungle' && pokemon.hasType("Bug")))  && // Jungle - Bug typed pokemon gain protosynthesis
+      (weather.includes('Sun') || pokemon.hasItem('Booster Energy')) ||
     (pokemon.hasAbility('Quark Drive') &&
       (terrain === 'Electric' || pokemon.hasItem('Booster Energy'))) ||
     (pokemon.boostedStat !== 'auto')
